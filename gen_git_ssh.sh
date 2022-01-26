b6df2781759aba6d5bcfee180030d8e9fcc984cd
bchandaka <bhargav2900@gmail.com>
2022-01-26 00:20:33 +0000

    Updated git ssh gen script

--- gen_git_ssh.sh
+++ gen_git_ssh.sh
@@ -1,6 +1,8 @@
 ssh-keygen -t ed25519 -C "bhargav2900@gmail.com"
 eval "$(ssh-agent -s)"
-ssh-add -K ~/.ssh/id_ed25519
+ssh-add ~/.ssh/id_ed25519
 
 echo Copy the ssh key below into github->settings->SSH and GPG Keys->New SSH Key
 cat ~/.ssh/id_ed25519.pub
+git config --global user.email "bhargav2900@gmail.com"
+git config --global user.name "bchandaka"
